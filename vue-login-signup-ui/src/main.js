3ffe218a6b54132ee771cd4a721e77cbaf2e3f3d
Edivan <capic.20191tads0326@aluno.ifpi.edu.br>
2022-10-07 09:16:47 -0300

    criando uma pasta e css e importando ela na main.js

--- vue-login-signup-ui/src/main.js
+++ vue-login-signup-ui/src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import '@/assets/css/main.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 createApp(App).use(router).mount('#app')
 
-import 'bootstrap/dist/css/bootstrap.min.css'
+
 Vue.config.productionTip = false
 new Vue({
   router,
