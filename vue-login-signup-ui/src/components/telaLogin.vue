6765cd6981ce6f84b4c4a64710f1f893db8f76a0
Edivan <capic.20191tads0326@aluno.ifpi.edu.br>
2022-10-07 10:23:06 -0300

    criando o projeto e suas depencias de vue

--- vue-login-signup-ui/src/components/telaLogin.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <h1>ola</h1>
-</template>
-
-<script>
-export default {
-    name:'telaLogin',
-}
-</script>
--- vue-login-signup-ui/src/main.js
+++ vue-login-signup-ui/src/main.js
@@ -1,12 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import '@/assets/css/main.css'
-import 'bootstrap/dist/css/bootstrap.min.css'
 
 createApp(App).use(router).mount('#app')
-Vue.config.productionTip = false
-new Vue({
-  router,
-  render: h => h(App)
-}).$mount('#app')
